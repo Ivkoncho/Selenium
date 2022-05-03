4e561d2a2168585d088e4ff95ae204472db261b4
Ivkoncho <51968910+Ivkoncho@users.noreply.github.com>
2022-05-03 22:03:59 +0300

    Initial commit

---
+++ src/test/java/net/progress/test/LoginAndRegistration.java
@@ -0,0 +1,11 @@
+package net.progress.test;
+
+import org.junit.Test;
+
+public class LoginAndRegistration {
+    @Test
+    public void sampleCSS(){
+     driver = DriverHelper.getDriver();
+     driver.get("http://practice.automationtesting.in/my-account/shop");
+    }
+}
